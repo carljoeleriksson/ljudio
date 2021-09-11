00f4f3732978176375416e880d29773d7c735483
Omar Mh <mr.omr86@gmail.com>
2021-09-11 12:02:24 +0200

    search bar/result

--- frontend/src/components/SearchRender.jsx
+++ frontend/src/components/SearchRender.jsx
@@ -3,20 +3,21 @@ import React from 'react'
 function SearchRender(result) {
 	//Getting prop (result) as object form some reason, so I redirect to the array that it contains.
 	const searchResult = result.result;
+	console.log(searchResult)
 
-	return 	(<>
-		<div className="song-result-container">
+	return (<>
+		<div className="song-result-container" >
 			<h2>Songs</h2>
 			<ul className="song-list">
 				{
-				searchResult.filter(content => content.type == "song")
-				.map(song => (
-				<li key={song.videoId}>
-					<p className="song-title">{song.name}</p>
-					<p className="artist-name">{song.artist.name}</p>
-					{/* playSong-button HERE */}
-				</li>
-				))
+					 searchResult.filter(content => content.type == "song")
+						.map(song => (
+							<li key={song.videoId}>
+								<p className="song-title">{song.name}</p>
+								<p className="artist-name">{song.artist.name}</p>
+								{/* playSong-button HERE */}
+							</li>
+						))
 				}
 			</ul>
 		</div>
@@ -25,12 +26,12 @@ function SearchRender(result) {
 			<h2>Artists</h2>
 			<ul className="artist-list">
 				{
-				searchResult.filter(content => content.type == "artist")
-				.map(artist => (
-				<li key={artist.browseId}>
-					<p className="artist-name">{artist.name}</p>
-				</li>
-				))
+					searchResult.length > 0 && searchResult.filter(content => content.type == "artist")
+						.map(artist => (
+							<li key={artist.browseId}>
+								<p className="artist-name">{artist.name}</p>
+							</li>
+						))
 				}
 			</ul>
 		</div>
@@ -38,12 +39,12 @@ function SearchRender(result) {
 			<h2>Albums</h2>
 			<ul className="album-list">
 				{
-				searchResult.filter(content => content.type == "album")
-				.map(album => (
-				<li key={album.browseId}>
-					<p className="album-name">{album.name}</p>
-				</li>
-				))
+					searchResult.length > 0 && searchResult.filter(content => content.type == "album")
+						.map(album => (
+							<li key={album.browseId}>
+								<p className="album-name">{album.name}</p>
+							</li>
+						))
 				}
 			</ul>
 		</div>
