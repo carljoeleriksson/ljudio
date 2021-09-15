acb7bc7db01ccdcd2e993c83b2f2a586144acf3f
Elie Asmar <elieasmar89@gmail.com>
2021-09-15 13:26:35 +0200

    login/register

--- frontend/src/components/FormSignup.jsx
+++ frontend/src/components/FormSignup.jsx
@@ -1,117 +1,115 @@
 import React, { useState } from 'react';
 import validate from './validateInfo';
 import { Link, Redirect } from 'react-router-dom';
+// import './FormSignup.css';
 
 function FormSignup() {
-   const [values, setValues] = useState({
-      firstname: '',
-      lastname: '',
-      email: '',
-      password: '',
-   });
+  const [values, setValues] = useState({
+    firstname: '',
+    lastname: '',
+    email: '',
+    password: '',
+  });
 
-   const [errors, setErrors] = useState({});
-   const [redirect, setRedirect] = useState(false);
+  const [errors, setErrors] = useState({});
+  const [redirect, setRedirect] = useState(false);
 
-   function handleFormInput(e) {
-      let newdata = { ...values };
-      newdata[e.target.id] = e.target.value;
-      setValues(newdata);
-      console.log(newdata);
-   }
+  function handleFormInput(e) {
+    let newdata = { ...values };
+    newdata[e.target.id] = e.target.value;
+    setValues(newdata);
+    console.log(newdata);
+  }
 
-   // async function handleFormSubmit(e) {
-   //    e.preventDefault();
-   //    setErrors(validate(values));
-   //    Axios.post('/api/registerMemeber', {
-   //       FirstName: values.firstname.trim(),
-   //       LastName: values.lastname.trim(),
-   //       Email: values.email.trim(),
-   //       Password: values.password.trim(),
-   //    }).then((res) => {
-   //       console.log(res);
-   //    });
-   //    setRedirect(true);
-   // }
+  // async function handleFormSubmit(e) {
+  //    e.preventDefault();
+  //    setErrors(validate(values));
+  //    Axios.post('/api/registerMemeber', {
+  //       FirstName: values.firstname.trim(),
+  //       LastName: values.lastname.trim(),
+  //       Email: values.email.trim(),
+  //       Password: values.password.trim(),
+  //    }).then((res) => {
+  //       console.log(res);
+  //    });
+  //    setRedirect(true);
+  // }
 
-   async function handleFormSubmit(e) {
-      e.preventDefault();
-      setErrors(validate(values));
+  async function handleFormSubmit(e) {
+    e.preventDefault();
+    setErrors(validate(values));
 
-      await fetch('/api/registerMemeber', {
-         method: 'POST',
-         headers: { 'Content-Type': 'application/json' },
-         body: JSON.stringify({
-            FirstName: values.firstname.trim(),
-            LastName: values.lastname.trim(),
-            Email: values.email.trim(),
-            Password: values.password.trim(),
-         }),
-      });
-      setRedirect(true);
-   }
+    await fetch('/api/registerMemeber', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        FirstName: values.firstname.trim(),
+        LastName: values.lastname.trim(),
+        Email: values.email.trim(),
+        Password: values.password.trim(),
+      }),
+    });
+    setRedirect(true);
+  }
 
-   if (redirect) {
-      return <Redirect to="/login" />;
-   }
+  if (redirect) {
+    return <Redirect to='/login' />;
+  }
 
-   return (
-      <div>
-         <form onSubmit={(e) => submit(e)}>
-            <h1>Create account</h1>
-            <div>
-               <input
-                  id="firstname"
-                  type="text"
-                  name="firstname"
-                  placeholder="Enter Your First Name"
-                  onChange={(e) => handleFormInput(e)}
-                  value={values.firstname}
-               />
+  return (
+    <main>
+      <img className='logo' src='../../assets/logo.svg' alt='Logo' />
+      <div className='form-container'>
+        <form onSubmit={(e) => submit(e)}>
+          <h1 className='form-title'>CREATE ACCOUNT</h1>
+          <input
+            id='firstname'
+            type='text'
+            name='firstname'
+            placeholder='Enter Your First Name'
+            onChange={(e) => handleFormInput(e)}
+            value={values.firstname}
+          />
 
-               {errors.firstname && <p>{errors.firstname}</p>}
-            </div>
-            <div>
-               <input
-                  id="lastname"
-                  type="text"
-                  name="lastname"
-                  placeholder="Enter Your Last Name"
-                  onChange={(e) => handleFormInput(e)}
-                  value={values.lastname}
-               />
-               {errors.lastname && <p>{errors.lastname}</p>}
-            </div>
-            <div>
-               <input
-                  id="email"
-                  type="email"
-                  name="email"
-                  placeholder="Enter Your Email"
-                  onChange={(e) => handleFormInput(e)}
-                  value={values.email}
-               />
-               {errors.email && <p>{errors.email}</p>}
-            </div>
-            <div>
-               <input
-                  id="password"
-                  type="password"
-                  name="password"
-                  placeholder="Enter Your Password"
-                  onChange={(e) => handleFormInput(e)}
-                  value={values.password}
-               />
-               {errors.password && <p>{errors.password}</p>}
-            </div>
+          {errors.firstname && <p className='error'>{errors.firstname}</p>}
+          <input
+            id='lastname'
+            type='text'
+            name='lastname'
+            placeholder='Enter Your Last Name'
+            onChange={(e) => handleFormInput(e)}
+            value={values.lastname}
+          />
+          {errors.lastname && <p className='error'>{errors.lastname}</p>}
+          <input
+            id='email'
+            type='email'
+            name='email'
+            placeholder='Enter Your Email'
+            onChange={(e) => handleFormInput(e)}
+            value={values.email}
+          />
+          {errors.email && <p className='error'>{errors.email}</p>}
+          <input
+            id='password'
+            type='password'
+            name='password'
+            placeholder='Enter Your Password'
+            onChange={(e) => handleFormInput(e)}
+            value={values.password}
+          />
+          {errors.password && <p className='error'>{errors.password}</p>}
 
-            <button onClick={handleFormSubmit}>Sign up</button>
-            <span>
-               Already have an account? Login <Link to="/login">Here</Link>
-            </span>
-         </form>
+          <button className='btn' onClick={handleFormSubmit}>
+            SIGN UP
+          </button>
+          <span>
+            Already have an account? Login <Link to='/login'>Here</Link>
+          </span>
+        </form>
       </div>
-   );
+    </main>
+  );
 }
 
 export default FormSignup;
--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -2,106 +2,107 @@ import React, { useRef, useState } from 'react';
 import validate from './validateLogin';
 
 function Login() {
-   const [values, setValues] = useState({
-      email: '',
-      password: '',
-   });
+  const [values, setValues] = useState({
+    email: '',
+    password: '',
+  });
 
-   const [errors, setErrors] = useState({});
+  const [errors, setErrors] = useState({});
 
-   function handleFormInput(e) {
-      let newdata = { ...values };
-      newdata[e.target.id] = e.target.value;
-      setValues(newdata);
-      console.log(newdata);
-   }
-   //saveToken = (token) => {
-   function saveToken(token) {
-      return new Promise((resolve, reject) => {
-         sessionStorage.setItem('auth', token);
+  function handleFormInput(e) {
+    let newdata = { ...values };
+    newdata[e.target.id] = e.target.value;
+    setValues(newdata);
+    console.log(newdata);
+  }
+  //saveToken = (token) => {
+  function saveToken(token) {
+    return new Promise((resolve, reject) => {
+      sessionStorage.setItem('auth', token);
 
-         resolve('Done');
-      });
-   }
-   async function handleFormSubmit(e) {
-      e.preventDefault();
-      setErrors(validate(values));
-      const response = await fetch('/api/login', {
-         method: 'POST',
-         headers: { 'Content-Type': 'application/json' },
-         body: JSON.stringify({
-            Email: values.email,
-            Password: values.password,
-         }),
-      });
-      const data = await response.json();
-      console.log(data);
+      resolve('Done');
+    });
+  }
+  async function handleFormSubmit(e) {
+    e.preventDefault();
+    setErrors(validate(values));
+    const response = await fetch('/api/login', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        Email: values.email,
+        Password: values.password,
+      }),
+    });
+    const data = await response.json();
+    console.log(data);
 
-      if (data) {
-         saveToken(data);
-      }
-   }
+    if (data) {
+      saveToken(data);
+    }
+  }
 
-   // get token from session storage
-   // getToken = () => {
-   //    let token = null;
+  // get token from session storage
+  // getToken = () => {
+  //    let token = null;
 
-   //    if (sessionStorage.getItem('auth')) {
-   //       token = sessionStorage.getItem('auth');
-   //    }
+  //    if (sessionStorage.getItem('auth')) {
+  //       token = sessionStorage.getItem('auth');
+  //    }
 
-   //    return token;
-   // };
+  //    return token;
+  // };
 
-   // // delete token from session storage
-   // deleteToken = () => {
-   //    sessionStorage.removeItem('auth');
-   // };
+  // // delete token from session storage
+  // deleteToken = () => {
+  //    sessionStorage.removeItem('auth');
+  // };
 
-   // function handleFormSubmit(e) {
-   //    e.preventDefault();
-   //    setErrors(validate(values));
-   //    Axios.post('/api/login', {
-   //       headers: { authorization: 'Bearer ' + user.id },
-   //       Email: values.email.trim(),
-   //       Password: values.password.trim(),
-   //    }).then((res) => {
-   //       console.log(res);
-   //    });
-   // }
+  // function handleFormSubmit(e) {
+  //    e.preventDefault();
+  //    setErrors(validate(values));
+  //    Axios.post('/api/login', {
+  //       headers: { authorization: 'Bearer ' + user.id },
+  //       Email: values.email.trim(),
+  //       Password: values.password.trim(),
+  //    }).then((res) => {
+  //       console.log(res);
+  //    });
+  // }
 
-   return (
-      <div>
-         <h1>Login</h1>
-         <form onSubmit={(e) => submit(e)}>
-            <div>
-               <input
-                  type="text"
-                  placeholder="Email"
-                  name="email"
-                  value={values.email}
-                  onChange={(e) => handleFormInput(e)}
-                  id="email"
-               />
-               {errors.email && <p>{errors.email}</p>}
-            </div>
-            <div>
-               <input
-                  type="password"
-                  placeholder="Password"
-                  name="password"
-                  value={values.password}
-                  onChange={(e) => handleFormInput(e)}
-                  id="password"
-               />
-               {errors.password && <p>{errors.password}</p>}
-            </div>
-            <div>
-               <button onClick={handleFormSubmit}>Login</button>
-            </div>
-         </form>
+  return (
+    <main>
+      <img className='logo' src='../../assets/logo.svg' alt='Logo' />
+      <div className='form-container'>
+        <h1 className='form-title'>LOGIN</h1>
+        <form onSubmit={(e) => submit(e)}>
+          <input
+            type='text'
+            placeholder='Email'
+            name='email'
+            value={values.email}
+            onChange={(e) => handleFormInput(e)}
+            id='email'
+          />
+          {errors.email && <p className='error'>{errors.email}</p>}
+
+          <input
+            type='password'
+            placeholder='Password'
+            name='password'
+            value={values.password}
+            onChange={(e) => handleFormInput(e)}
+            id='password'
+          />
+          {errors.password && <p className='error'>{errors.password}</p>}
+
+          <button className='btn' onClick={handleFormSubmit}>
+            Login
+          </button>
+        </form>
       </div>
-   );
+    </main>
+  );
 }
 
 export default Login;
