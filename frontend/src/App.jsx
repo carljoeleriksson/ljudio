9a730ca9288dda181ef7af957b3b511b3e09b6cd
carljoeleriksson <carljoeleriksson@gmail.com>
2021-09-28 14:01:00 +0200

    reverting to old dev

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,65 +15,13 @@ import './App.css';
 
 function App() {
 
-   const [searchResult, setSearchResult] = useState();
-   const [searchTerm, setSearchTerm] = useState('');
-   const [searchType, setSearchType] = useState('search');
-
-   async function fetchSearchResult(e) {
-      e.preventDefault();
-      console.log('searchType: ' + searchType);
-
-      const response = await fetch('/api/search', {
-         method: 'POST',
-         body: JSON.stringify({
-            searchType: searchType,
-            keyWord: searchTerm,
-         }),
-         headers: {
-            'Content-Type': 'application/json',
-         },
-      });
-      const data = await response.json();
-
-      if (data) {
-         setSearchResult(data.content);
-      } else {
-         console.log('Failed to fetch. Got no data from backend.');
-      }
-   }
-
    return (
       <PlayerContextProvider>
          <Router>
             <Route path="/login" component={Login} />
             <Route path="/registerMember" component={Form} />
-         <div id="wrapper">   
             <Route path="/" exact component={HomePage} />
-
-         <header>
-            <img className="logo-header" src="/assets/logo.svg" alt="Logo" />
-            <SearchBar
-               onChange={(e) => {
-                  console.log(e.target.value);
-
-                  if (e.target.type == 'text') {
-                     setSearchTerm(e.target.value);
-                  } else {
-                     setSearchType(e.target.value);
-                  }
-               }}
-               onClick={fetchSearchResult}
-            />
-         </header>
-         <main>
-            <SearchRender result={searchResult} />
             <Route path="/singleplaylistpage" component={SinglePlaylistPage} />
-         </main>
-         <aside id="playlist-sidebar">
-         <Playlists></Playlists>
-         </aside>
-         <Player></Player>
-      </div>
       </Router>
       </PlayerContextProvider>
    );
