9135dae7927ef223ad6c9a9f4c4b7ca76d2c5db3
Omar Mh <mr.omr86@gmail.com>
2021-09-29 07:33:27 +0200

    redirect logn

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
-import React, { useState, useContext } from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Link, Redirect } from 'react-router-dom';
 import Form from './components/Form';
 import Login from './components/Login';
 import HomePage from './pages/HomePage';
@@ -15,14 +15,34 @@ import './App.css';
 
 function App() {
 
+   const [redirect, setRedirect] = useState(false);
+
+   function nologin_redirect() {
+
+      let token = sessionStorage.getItem('auth');
+
+      if (!token) {
+
+         setRedirect(true);
+
+      }
+
+   }
+   useEffect(() => {
+
+      nologin_redirect();
+
+   }, []);
+
    return (
       <PlayerContextProvider>
          <Router>
+            {redirect && <Redirect to='/login' />}
             <Route path="/login" component={Login} />
             <Route path="/registerMember" component={Form} />
             <Route path="/" exact component={HomePage} />
             <Route path="/singleplaylistpage" component={SinglePlaylistPage} />
-      </Router>
+         </Router>
       </PlayerContextProvider>
    );
 }
