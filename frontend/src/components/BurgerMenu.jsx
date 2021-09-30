e26d00ff9752722deae8aa323c1053bd5838f3bc
Ace1313 <alex.cadjo@hotmail.com>
2021-09-30 13:55:05 +0200

    logoutButton

--- frontend/src/components/BurgerMenu.jsx
+++ frontend/src/components/BurgerMenu.jsx
@@ -1,63 +1,73 @@
 import { color } from '@mui/system';
-import React from 'react'
-import { slide as Menu } from 'react-burger-menu'
+import React, { useState } from 'react';
+import { Redirect } from 'react-router-dom';
+import { slide as Menu } from 'react-burger-menu';
 import Playlists from './Playlists';
 
 const styles = {
-	bmBurgerButton: {
-	  position: 'fixed',
-	  width: '26px',
-	  height: '20px',
-	  left: '15px'
-	},
-	bmBurgerBars: {
-	  background: '#01a5af'
-	},
-	bmBurgerBarsHover: {
-	  background: '#a90000'
-	},
-	bmCrossButton: {
-	  height: '24px',
-	  width: '24px'
-	},
-	bmCross: {
-	  background: '#fff'
-	},
-	bmMenuWrap: {
-	  position: 'fixed',
-	  height: '100%'
-	},
-	bmMenu: {
-	  background: 'rgba(0, 0, 0, 0.9)',
-	  padding: '0 1.5em 0 0',
-	  fontSize: '1.15em'
-	},
-	bmMorphShape: {
-	  fill: '#373a47'
-	},
-	bmItemList: {
-	  color: '#b8b7ad',
-	  padding: '0.8em',
-	  
-	},
-	bmItem: {
-	  display: 'inline-block'
-	},
-	bmOverlay: {
-	  background: 'rgba(0, 0, 0, 0.3)'
-	}
-  }
+   bmBurgerButton: {
+      position: 'fixed',
+      width: '26px',
+      height: '20px',
+      left: '15px',
+   },
+   bmBurgerBars: {
+      background: '#01a5af',
+   },
+   bmBurgerBarsHover: {
+      background: '#a90000',
+   },
+   bmCrossButton: {
+      height: '24px',
+      width: '24px',
+   },
+   bmCross: {
+      background: '#fff',
+   },
+   bmMenuWrap: {
+      position: 'fixed',
+      height: '100%',
+   },
+   bmMenu: {
+      background: 'rgba(0, 0, 0, 0.9)',
+      padding: '0 1.5em 0 0',
+      fontSize: '1.15em',
+   },
+   bmMorphShape: {
+      fill: '#373a47',
+   },
+   bmItemList: {
+      color: '#b8b7ad',
+      padding: '0.8em',
+   },
+   bmItem: {
+      display: 'inline-block',
+   },
+   bmOverlay: {
+      background: 'rgba(0, 0, 0, 0.3)',
+   },
+};
 
 function BurgerMenu() {
-	const showSettings = (e) => {
-		e.preventDefault();
-	}
-	return (
-		<Menu styles={ styles } width={'280px'} >
-			<h5 className="burger-menu-title">Playlists</h5>
-			<Playlists></Playlists>
-      	</Menu>
-	)
+   const [redirect, setRedirect] = useState(false);
+   const showSettings = (e) => {
+      e.preventDefault();
+   };
+
+   function logout() {
+      console.log('Hello');
+      sessionStorage.removeItem('auth');
+      setRedirect(true);
+   }
+
+   return (
+      <Menu styles={styles} width={'280px'}>
+         <h5 className="burger-menu-title">Playlists</h5>
+         <Playlists></Playlists>
+         <button onClick={logout}>Logout</button>
+         {redirect && <Redirect to="/Login" />}
+      </Menu>
+   );
 }
 
-export default BurgerMenu
+export default BurgerMenu;
